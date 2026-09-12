1bde86dcbef1851804ba0dbf5d682bf8358c86b2
tejaswini2030grad <tejapalvadi@gmail.com>
2026-09-12 23:07:24 +0530

    Create pattern.c

---
+++ pattern.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a=1;
+    char ch;
+    printf("enter a character:");
+    scanf("%c",&ch);
+    printf("%d %d %d %d %d %d\n",a,a,a,a,a,a);
+    printf("%d    %c    %d\n",a,ch,a);
+    printf("%d    %c    %d\n",a,ch,a);
+    printf("%d    %c    %d\n",a,ch,a);
+    printf("%d    %c    %d\n",a,ch,a);
+    printf("%d    %c    %d\n",a,ch,a);
+    printf("%d %d %d %d %d %d\n",a,a,a,a,a,a);
+    return 0;
+}
